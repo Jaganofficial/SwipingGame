22fc98988baa421183edc40a190c8c6aabfd2b2d
Jagan <jaganeeshwar2002@gmail.com>
2022-04-07 10:24:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/application1/Adapter.java
@@ -0,0 +1,60 @@
+package com.example.application1;
+
+import android.content.Context;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    private List<Modelcalss>userlist;
+    private Context context;
+
+    Adapter(List<Modelcalss> userlist, Context context)
+    {
+        this.context=context;
+        this.userlist=userlist;
+    }
+
+    @NonNull
+    @org.jetbrains.annotations.NotNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.bars,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull Adapter.ViewHolder holder, int position) {
+        String Color = userlist.get(position).getColor();
+        if (Color.equals("Yellow"))
+        {
+           holder.linearLayout.setBackgroundTintList(context.getResources().getColorStateList(R.color.Yellow));
+        }
+        else if(Color.equals("Red"))
+        {
+            holder.linearLayout.setBackgroundTintList(context.getResources().getColorStateList(R.color.Red));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return userlist.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private LinearLayout linearLayout;
+        public ViewHolder(@NonNull @org.jetbrains.annotations.NotNull View itemView) {
+            super(itemView);
+            linearLayout=itemView.findViewById(R.id.bars);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/application1/Modelcalss.java
@@ -0,0 +1,13 @@
+package com.example.application1;
+
+public class Modelcalss  {
+    String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public Modelcalss(String color) {
+        this.color = color;
+    }
+}
